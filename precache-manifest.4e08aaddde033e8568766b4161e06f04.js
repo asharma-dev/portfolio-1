b0200c6536fbcb38b7a219a199ab7c3ad631a7ca
Dalbir Rana <dalbirsrana@gmail.com>
2020-11-30 15:22:16 -0800

    Updates

---
+++ precache-manifest.4e08aaddde033e8568766b4161e06f04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06a6e6d63a177c1831e1cfd4391e435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33e78a2fad53b683609c",
+    "url": "/static/css/main.17ac5a42.chunk.css"
+  },
+  {
+    "revision": "982df88d3c38ce6c3bf6",
+    "url": "/static/js/2.dee89ef5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dee89ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e78a2fad53b683609c",
+    "url": "/static/js/main.7ee82f15.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  },
+  {
+    "revision": "a46e500df609ead253c6698da4d16ce1",
+    "url": "/static/media/pattern-geometry-2.a46e500d.png"
+  }
+]);
